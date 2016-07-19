b8823f48a8b7a39e1086d1f6e053fd00025fad33
Dan Schuman <danschuman@gmail.com>
2016-07-18 21:08:06 -0700

    We did stuff!

--- day-1/main.js
+++ day-1/main.js
@@ -1,4 +1,56 @@
-const rootNode = document.getElementById('myRootNode');
-const myElement = React.createElement('div', null, 'Hello, world!');
+class MyWordList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
 
-ReactDOM.render(myElement, rootNode);
+  clickHandler(e) {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  render() {
+    const wordList = ['world', 'dog', 'dog', 'cat'].map((word, idx) => {
+      return (
+        <li key={idx}>{this.props.prefix} Hello {word}!</li>
+      );
+    });
+
+    const onChangeHandler = (e) => {
+      console.log('New value:', e.target.value);
+      this.setState({ count: this.state.count + 1 });
+    };
+
+    return (
+      <div
+        style={{ border: '1px solid black', margin: 5 }}
+        onClick={this.clickHandler.bind(this)}>
+
+        The count is <input type='checkbox' checked={this.state.count % 2 === 0} onChange={onChangeHandler}/> {this.state.count}.
+
+        <ul style={{ backgroundColor: '#44f' }}>
+          {wordList}
+        </ul>
+      </div>
+    );
+  }
+}
+
+MyWordList.propTypes = {
+  prefix: React.PropTypes.string.isRequired,
+};
+
+function getElement() {
+  return (
+    <div>
+      <MyWordList prefix='Uhhhhhh'/>
+      <MyWordList prefix='Bahhh'/>
+      <MyWordList prefix='Deee'/>
+
+
+    </div>
+  );
+}
+
+const rootNode = document.getElementById('myRootNode');
+ReactDOM.render(getElement(), rootNode);
+
