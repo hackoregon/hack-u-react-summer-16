8e4930e93039eea16d5118ff5ff054fef0586e21
Dan Schuman <danschuman@gmail.com>
2016-07-27 13:39:58 -0700

    Add URL on startup

--- boilerplate/webpack/webpack-dev-server.js
+++ boilerplate/webpack/webpack-dev-server.js
@@ -40,7 +40,7 @@ app.listen(port, function onAppListening(err) {
   if (err) {
     console.error(err);
   } else {
-    console.info('==> 🚧  Webpack development server listening on port %s', port);
+    console.info('==> 🚧  Webpack development server listening at http://localhost:%s', port);
   }
   /* eslint-enable no-console */
 });
