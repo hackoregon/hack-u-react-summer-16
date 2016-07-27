7b13a04c717c74be5b4d8780bab403a944a60a50
Dan Schuman <danschuman@gmail.com>
2016-07-27 12:42:51 -0700

    Add bootstrap

--- boilerplate/src/components/App.js
+++ boilerplate/src/components/App.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>Hello, world!</div>
+      <div className='container'>
+        <h1>Hello, world!</h1>
+        <p>Let's get funky.</p>
+      </div>
     );
   }
 }
