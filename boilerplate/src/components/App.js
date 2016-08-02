2d9508d34798c36b27982f9d29fc5a9acb1a4902
Dan Schuman <danschuman@gmail.com>
2016-08-01 20:24:43 -0700

    Counter done!

--- boilerplate/src/components/App.js
+++ boilerplate/src/components/App.js
@@ -4,8 +4,39 @@ export default class App extends Component {
   render() {
     return (
       <div className='container'>
-        <h1>Hello, world!</h1>
-        <p>Let's get funky.</p>
+        <Counter />
+        <Counter by={2} />
+        <Counter by={3} />
+        <Counter by={4} />
+      </div>
+    );
+  }
+}
+
+class Counter extends Component {
+  static propTypes = {
+    by: React.PropTypes.number
+  };
+
+  static defaultProps = {
+    by: 1,
+  };
+
+  state = {
+    count: 1,
+  };
+
+  onClick(e) {
+    this.setState({
+      count: this.state.count + this.props.by,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <input type='number' value={this.state.count} readOnly />
+        <button onClick={this.onClick.bind(this)}>Increment by {this.props.by}</button>
       </div>
     );
   }
