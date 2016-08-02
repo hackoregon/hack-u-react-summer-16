6f6f57c07c6611ec6989cb27c4952c8ac7cff9a1
Dan Schuman <danschuman@gmail.com>
2016-08-01 19:43:58 -0700

    Done!

--- boilerplate/src/components/App.js
+++ boilerplate/src/components/App.js
@@ -3,9 +3,49 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
+      <div>
+        <ColorChanger colors={['red', 'orange', 'silver']}/>
+        <ColorChanger/>
+        <ColorChanger/>
+        <ColorChanger/>
+      </div>
+    );
+  }
+}
+
+class ColorChanger extends Component {
+  state = {
+    colorIdx: 0,
+  };
+
+  static defaultProps = {
+    colors: [
+      'green',
+      'blue',
+      'pink',
+      '#44ff44',
+      'orange',
+    ],
+  }
+
+  render() {
+    const colorStyle = {
+      backgroundColor: this.props.colors[this.state.colorIdx],
+      width: 50,
+      height: 50,
+    };
+
+    return (
       <div className='container'>
-        <h1>Hello, world!</h1>
-        <p>Let's get funky.</p>
+        <div style={colorStyle}>
+        </div>
+        <button onClick={e => {
+          this.setState({
+            colorIdx: (this.state.colorIdx + 1) % this.props.colors.length,
+          });
+        }}>
+          Change Color
+        </button>
       </div>
     );
   }
