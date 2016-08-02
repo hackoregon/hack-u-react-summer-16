6e224e1a07b4290a20925e747c2fe0a864ce7847
Dan Schuman <danschuman@gmail.com>
2016-08-01 18:47:42 -0700

    Adder code

--- boilerplate/src/components/App.js
+++ boilerplate/src/components/App.js
@@ -1,11 +1,49 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+  state = {
+    addedValue: '',
+    num1: 1,
+    num2: 2,
+  };
+
   render() {
     return (
       <div className='container'>
-        <h1>Hello, world!</h1>
-        <p>Let's get funky.</p>
+        <input
+          onChange={e => {
+            this.setState({
+              num1: e.target.value
+            });
+          }}
+          ref='num1'
+          type='number'
+          value={this.state.num1}
+        />
+        +
+        <input
+          onChange={e => {
+            this.setState({
+              num2: e.target.value
+            });
+          }}
+          ref='num2'
+          type='number'
+          value={this.state.num2} />
+
+        <br/>
+
+        <button onClick={e => {
+          this.setState({
+            addedValue: this.state.num1 + this.state.num2,
+          });
+
+        }}>Add!</button>
+
+        <div>
+          Result:
+          <input readOnly type='number' value={this.state.addedValue} />
+        </div>
       </div>
     );
   }
