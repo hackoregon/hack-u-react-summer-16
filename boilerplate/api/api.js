c0c12e0023620827edefe377f1327bfa183138f6
Dan Schuman <danschuman@gmail.com>
2016-08-08 13:34:22 -0700

    Basic blog API

---
+++ boilerplate/api/api.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-console */
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const posts = [];
+const defaultPost = {
+  id: null,
+  author: 'Unknown Author',
+  title: 'Post Title',
+  body: 'Post Content',
+};
+
+let _nextId = 1;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/posts/new', function(req, res) {
+  const newPost = { ...defaultPost, ...req.body, id: _nextId++ };
+  posts.push(newPost);
+  res.json(newPost);
+});
+
+app.get('/posts', function(req, res) {
+  res.json(posts.map(post => ({ ...defaultPost, ...post })));
+});
+
+app.listen(9999, function() {
+  console.log('Example app listening on port 9999!');
+});
