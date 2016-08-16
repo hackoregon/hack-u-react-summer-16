134cd65578072857aa4b3b26e7a5bf6c41114079
Dan Schuman <danschuman@gmail.com>
2016-08-15 20:32:15 -0700

    Add DevTools

--- boilerplate/src/client.js
+++ boilerplate/src/client.js
@@ -4,9 +4,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // redux
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+// Exported from redux-devtools
+import { createDevTools } from 'redux-devtools';
+
+// Monitors are separate packages, and you can make a custom one
+import LogMonitor from 'redux-devtools-log-monitor';
+import DockMonitor from 'redux-devtools-dock-monitor';
 
 import { Router, Route, browserHistory, IndexRedirect } from 'react-router';
 
@@ -23,20 +29,38 @@ class NotFound extends React.Component {
   }
 }
 
-let store = createStore(reducer, undefined, applyMiddleware(thunk));
+// createDevTools takes a monitor and produces a DevTools component
+const DevTools = createDevTools(
+  // Monitors are individually adjustable with props.
+  // Consult their repositories to learn about those props.
+  // Here, we put LogMonitor inside a DockMonitor.
+  // Note: DockMonitor is visible by default.
+  <DockMonitor toggleVisibilityKey='ctrl-h'
+               changePositionKey='ctrl-q'
+               defaultIsVisible={false}>
+    <LogMonitor theme='solarized' />
+  </DockMonitor>
+);
+let store = createStore(reducer, undefined, compose(
+  applyMiddleware(thunk),
+  DevTools.instrument(),
+));
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={browserHistory}>
-      <Route path='/' component={App}>
-        <IndexRedirect to='home' />
-        <Route path='about' component={About} />
-        <Route path='home' component={Home}/>
-        <Route path='posts/:id' component={Posts} />
+    <div>
+      <Router history={browserHistory}>
+        <Route path='/' component={App}>
+          <IndexRedirect to='home' />
+          <Route path='about' component={About} />
+          <Route path='home' component={Home}/>
+          <Route path='posts/:id' component={Posts} />
 
-        <Route path='*' component={NotFound} />
-      </Route>
-    </Router>
+          <Route path='*' component={NotFound} />
+        </Route>
+      </Router>
+      <DevTools />
+    </div>
   </Provider>,
   document.getElementById('content'),
 );
